186193cebdec5bafb1e8ff1158e10fe773be08de
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-01-31 00:13:44 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //This interface implements data from database
+    public interface IEntity
+    {
+
+    }
+}
