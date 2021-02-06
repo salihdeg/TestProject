96e5e0e72b21ff508bba51e5e76837898df64add
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-02-06 23:58:59 +0300

    Camp Day 9 - 06.02.2021

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //This interface implements data from database
     public interface IEntity
--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -1,4 +1,4 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
