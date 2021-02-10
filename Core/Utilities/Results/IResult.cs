57a3a9bbd87653c76f63d84de5d32d32d9bfb20f
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-02-11 00:03:53 +0300

    Camp Day 10 - 11.02.2021

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //For the base voids
+    public interface IResult
+    {
+        bool Success { get; }
+        string Message { get; }
+    }
+}
